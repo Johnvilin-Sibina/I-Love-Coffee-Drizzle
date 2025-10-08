766bc149e1c272618e7db9c3e8999acd8937b9e9
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-10-08 17:18:16 +0530

    Added try-catch

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -24,31 +24,51 @@ export class CoffeesService {
   }
 }
   async findAll() {
-    return await this.db.select().from(coffeesTable);
+   try {
+    const coffees = await this.db.select().from(coffeesTable)
+     return coffees;
+   } catch (error) {
+    console.error('Error fetching coffees:', error);
+    throw error;
+   }
   }
 
   async findOne(id: number) {
-    const result = await this.db
+    try {
+      const coffee = await this.db
       .select()
       .from(coffeesTable)
       .where(eq(coffeesTable.id, id));
-    return result[0];
+    return coffee[0];
+    } catch (error) {
+      console.error('Error fetching coffee:', error);
+      throw error;
+    }
   }
 
   async update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
-    const result = await this.db
+    try {
+      const updatedCoffee = await this.db
       .update(coffeesTable)
       .set(updateCoffeeDto)
       .where(eq(coffeesTable.id, id))
       .returning();
-    return result[0];
+    return updatedCoffee[0];
+    } catch (error) {
+      console.error('Error updating coffee:', error);
+      throw error;
+    }
   }
 
   async remove(id: number) {
-    const result = await this.db
+    try {
+      const result = await this.db
       .delete(coffeesTable)
       .where(eq(coffeesTable.id, id))
       .returning();
-    return result[0];
+    } catch (error) {
+      console.error('Error deleting coffee:', error);
+      throw error;
+    }
   }
 }
